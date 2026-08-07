cb3df36864fcc14cf50b8b894b382e8937cafd05
MuratYucedag <yucedagmurat23@gmail.com>
2026-08-07 21:07:25 +0300

    Add project files.

---
+++ SalePredict/Entities/Sale.cs
@@ -0,0 +1,17 @@
+﻿namespace SalePredict.Entities
+{
+    public class Sale
+    {
+        public int SaleId { get; set; }
+        public DateTime SaleDate { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int SalesQuantity { get; set; }
+        public int TotalQuantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Gender { get; set; }
+        public string PaymentMethod { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+}
